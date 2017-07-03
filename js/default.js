99ff3c87a282751323fa7192796458cfab95c146
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-07-03 18:05:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -76,3 +76,12 @@ function getCookie(c_name){
  
 }
 
+var last_date = getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie').textContent = '前回訪れた時間：' + last_date;
+}else{
+  document.getElementById('cookie').textContent = 'はじめまして';
+}
+
+var current_time = new Date();
+setCookie('lastDate', current_time.toString(), 7);
